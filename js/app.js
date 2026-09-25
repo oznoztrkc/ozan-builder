e71fd49834b4f84e194a3141f6ffa9bb816b679f
oznoztrkc <cxdtisp@gmail.com>
2026-09-25 23:21:16 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Ozan Builder ana uygulama dosyası
+
+const OzanBuilder = {
+
+  version: "1.0.0",
+
+  init() {
+    console.log("Ozan Builder başlatıldı.");
+  }
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  OzanBuilder.init();
+});
